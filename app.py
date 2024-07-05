587fc5794df106f71da7e3d9225331a3622b5b0e
NicolasZF <102821750+NicolasZFr@users.noreply.github.com>
2024-07-05 15:46:18 -0500

    Se corrigen errores

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-# Iniciar Flask
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
-def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("greet.html",name=name)
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
